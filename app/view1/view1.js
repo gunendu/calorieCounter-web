c7bc55e691b8cc941104dcc76f124a4d28429f64
gunendu <gunnedu@gmail.com>
2017-07-17 14:42:11 +0530

    filter and searching done

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,37 +1,38 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+var myApp = angular.module('myApp.view1', ['ngRoute'])
 
-.controller('View1Ctrl', ['$scope', '$http',  function($scope,$http){
-    console.log("this is called");
-    $http.get('http://localhost:9000/category')
-        .then(
+myApp.controller('View1Ctrl', ['$scope', '$http',  function($scope,$http){
+    console.log("this is called");    
+    $scope.init = function() {
+        
+        $http.get('http://localhost:9000/category')
+                .then(
+                        function(response){                              
+                                $scope.foodcategory = angular.copy(response.data);
+                        }),
+                        function(err){
+                                console.log("error is",err);    
+                                return $q.reject(err)
+                        }
+        $http.get('http://localhost:9000/category/foods')
+                .then(
                 function(response){
-                          console.log(response.data);
-                          $scope.foodcategory = response.data;
+                        $scope.foods = response.data;
+                        $scope.tempList = response.data;
                 }),
                 function(err){
                         console.log(err);
                 }
-      $http.get('http://localhost:9000/category/foods')
-          .then(
-                function(response){
-                        $scope.foods = response.data;
-                        console.log("nutrition",$scope.foods);
-
-                }),
-                function(err){
-                    console.log(err);
-                }
-
-        $scope.selectedId   = function(cat){
-             console.log("selected cat",$scope.foodcategory);          
-             var categoryId  = $scope.foodcategory.CategoryId;     
-             console.log("selected field",categoryId); 
-             $scope.foods = _.filter($scope.foods,function(food){
-                  return  food.CategoryId == categoryId;
-             })
-
+    }
+    
+   $scope.selectedId   = function(categoryId){
+        if(categoryId){
+                $scope.tempList = _.filter($scope.foods,function(food){
+                        return  food.CategoryId == categoryId;   
+                })
         }
+   }
+   $scope.init();
 
 }]);
